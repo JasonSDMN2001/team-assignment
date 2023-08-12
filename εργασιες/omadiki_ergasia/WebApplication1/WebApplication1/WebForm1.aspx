3639fed341daa9f01fe691eaededad38c8472fcf
IOANNIS IASON BOIDANIS <61283674+JasonSDMN2001@users.noreply.github.com>
2023-08-12 14:44:06 +0300

    oh

---
+++ εργασιες/omadiki_ergasia/WebApplication1/WebApplication1/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    	<asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Προβολή" BackColor="#66FFFF" Font-Bold="True" />
+		<asp:Button ID="Button2" runat="server" BackColor="#66FFFF" Font-Bold="True" OnClick="Button2_Click" Text="Πλήθος Ανδρών" />
+        <asp:Button ID="Button3" runat="server" BackColor="#66FFFF" Font-Bold="True" OnClick="Button3_Click" Text="Πλήθος Γυναικών" />
+		<p>
+			<asp:Label ID="Label1" runat="server" Font-Size="X-Large" Text="Προβολή καταγεγραμένων κρουσμάτων:" BackColor="#66FF33"></asp:Label>
+		</p>
+        <asp:Label ID="Label2" runat="server" Text="Label"></asp:Label>
+        <p>
+            <asp:Label ID="Label3" runat="server" Text="Το πλήθος ανδρών είναι:"></asp:Label>
+        </p>
+        <asp:Label ID="Label4" runat="server" Text="Label"></asp:Label>
+        <p>
+            <asp:Label ID="Label5" runat="server" Text="Το πλήθος γυναικών είναι:"></asp:Label>
+        </p>
+        <asp:Label ID="Label6" runat="server" Text="Label"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ εργασιες/omadiki_ergasia/WebApplication1/WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        Class1 myObj = new Class1(); //δημιουργώ ένα αντικείμενο της τάξης Class1 έξω από αυτήν ώστε να μπορώ παρακάτω να καλώ τις μεθόδους της
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label2.Text = myObj.SelectCases();
+            Label3.Visible = false;
+            Label4.Visible = false;
+            Label5.Visible = false;
+            Label6.Visible = false;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label1.Visible = true;
+            Label2.Visible = true;
+            Label3.Visible = false;
+            Label4.Visible = false;
+            Label5.Visible = false;
+            Label6.Visible = false;
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Label1.Visible = false;
+            Label2.Visible = false;
+            Label3.Visible = true;
+            Label4.Visible = true;
+            Label4.Text = myObj.Andres().ToString();
+            Label5.Visible = false;
+            Label6.Visible = false;
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Label1.Visible = false;
+            Label2.Visible = false;
+            Label3.Visible = false;
+            Label4.Visible = false;
+            Label5.Visible = true;
+            Label6.Visible = true;
+            Label6.Text = myObj.Ginaikes().ToString();
+        }
+    }
+}
